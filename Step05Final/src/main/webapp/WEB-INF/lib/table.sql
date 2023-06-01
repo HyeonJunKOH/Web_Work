73e8532b401e1f12beee80e9c872ac521c2a7f9f
KOHHyeonJun <tttmmm100@naver.com>
2023-06-01 18:08:40 +0900

    Step05 6월1일 내용 파일 추가

--- Step05Final/src/main/webapp/WEB-INF/lib/table.sql
+++ Step05Final/src/main/webapp/WEB-INF/lib/table.sql
@@ -6,3 +6,16 @@ create table users(
 	profile varchar2(100), -- 프로필 이미지 경로를 저장할 칼럼
 	regdate date
 );
+
+-- 업로드된 파일의 정보를 저장할 테이블
+create table board_file(
+	num number primary key,
+	writer varchar2(100) not null,
+	title varchar2(100) not null,
+	orgFileName varchar2(100) not null, -- 원본 파일명
+	saveFileName varchar2(100) not null,-- 서버에 실제로 저장된 파일명
+	fileSize number not null, --파일의 크기
+	regdate date
+);
+
+create sequence board_file_seq;
